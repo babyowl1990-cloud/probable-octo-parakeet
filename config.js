4c4d5d5d2130d9bdcf474121a78a9019216d222e
babyowl1990-cloud <babyowl1990@gmail.com>
2026-09-20 23:46:44 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Which site this extension serves. Change it with:  python3 set_site.py <origin> <path>
-// (that also updates host_permissions in manifest.json, which must match).
-const CONFIG = {
-  origin: "http://localhost:8080",
-  path: "/",
-};
